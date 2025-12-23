48b18134b086fbc22143cadd7de0b471b649f803
darklight-amit <amit.rajawat12@gmail.com>
2025-12-23 11:44:34 +0000

    added first component

---
+++ learn-react/src/component/fetchApiData.js
@@ -0,0 +1,14 @@
+// this will fetch data from https://jsonmock.hackerrank.com/api/inventory 
+// and load it into table
+
+function HackerrankData(){
+    return (
+        <div>
+        <p>
+            This will fetch data from url "https://jsonmock.hackerrank.com/api/inventory".
+        </p>
+    </div>
+    );
+}
+
+export default HackerrankData; 

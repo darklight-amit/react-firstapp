67c9e56755442049459886ce24ca17468c9cc457
darklight-amit <amit.rajawat12@gmail.com>
2026-05-19 01:04:03 +0000

    added onsubmit handling

--- just-react/src/EnrolemenetForm.js
+++ just-react/src/EnrolemenetForm.js
@@ -1,25 +1,38 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function EnrolementForm(){
 
+        const [firstName, setFirstName] = useState("");
+        const [lastName, setLastName] = useState(" ");
+        const [welcomeMsg, setWelcomeMsg] = useState(" ");
+
+        const handleSubmit = (event) => {
+            setWelcomeMsg(`Welcome ${firstName} ${lastName}');
+            
+            event.preventDefault();
+        };
+    
+
     return (
-        <div className='enrolForm'>
+        <div className='enrolForm' onSubmit={handleSubmit}>
             
         
             <form>
             <h3>Student Enrolement Form</h3>
             <label>First Name:</label>
-            <input type='text' placeholder='First Name' />
+            <input type='text' placeholder='First Name'  onBlur={(event) => setFirstName(event.target.value)}/>
             <br />
             <br />
             <label>Last Name:</label>
-            <input type='text' placeholder='Last Name' />
+            <input type='text' placeholder='Last Name'  onBlur={(event)=>setFirstName(event.target.value)}/>
             <br />
             <br />
             <input type='submit'  value = 'Submit' />       
-            <label id="studentMsg" className="message"></label> 
             </form>
+            <label id="studentMsg" className="message">mylable</label> 
+
             
         </div>
     )
